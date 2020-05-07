7c686fb09c39839bc8df59d81028b585c72f21c4
certiprince <57842626+certiprince@users.noreply.github.com>
2020-05-07 13:42:29 +0100

    Cleaned up comments

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -12,12 +12,6 @@ m = length(y); % number of training examples
 J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-
 % Compute hypothesis
 hyp = sigmoid(X*theta);
 
