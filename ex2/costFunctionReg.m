16a46ff39fafd847e849d749e3573b3ea5f4cff0
certiprince <57842626+certiprince@users.noreply.github.com>
2020-05-07 13:29:05 +0100

    Cleaned up comments

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -7,15 +7,15 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% Initialise dependent variables
 J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
+% Compute the cost of a particular choice of theta.
+%   Set J to the cost.
+%   Compute the partial derivatives and set grad to the partial
+%   derivatives of the cost w.r.t. each parameter in theta.
+
 hyp = sigmoid(X*theta);
 theta(1) = 0;
 J = (1/m)*sum(-y'*log(hyp)-(1-y')*log(1-hyp)) + (lambda/(2*m)) * theta'*theta;
