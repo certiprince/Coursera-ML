4a40ac1220a4ce31f5ec7ca2a75b3788f24f36bb
certiprince <57842626+certiprince@users.noreply.github.com>
2020-05-07 13:25:29 +0100

    Cleaned up comments

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -4,28 +4,22 @@ function [J, grad] = costFunction(theta, X, y)
 %   parameter for logistic regression and the gradient of the cost
 %   w.r.t. to the parameters.
 
-% Initialize some useful values
+% Initialise some useful values
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
-
+% Compute hypothesis
 hyp = sigmoid(X*theta);
+
+% Compute the cost
 J = (1/m)*sum(-y'*log(hyp)-(1-y')*log(1-hyp));
 
+% Compute the gradient to the partial
+% derivatives of the cost w.r.t. each parameter in theta
 grad = (1/m)*X'*(hyp - y);
-
-
 
 % =============================================================
 
