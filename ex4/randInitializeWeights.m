fd587035e3ba0a698a10364c1a9089a429e39b1f
certiprince <57842626+certiprince@users.noreply.github.com>
2020-05-07 13:55:13 +0100

    Cleaned up comments

--- ex4/randInitializeWeights.m
+++ ex4/randInitializeWeights.m
@@ -7,26 +7,17 @@ function W = randInitializeWeights(L_in, L_out)
 %
 %   Note that W should be set to a matrix of size(L_out, 1 + L_in) as
 %   the first column of W handles the "bias" terms
-%
 
-% You need to return the following variables correctly 
+% Initialise W 
 W = zeros(L_out, 1 + L_in);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Initialize W randomly so that we break the symmetry while
-%               training the neural network.
+% Initialize W randomly to break symmetry while
+% training the neural network.
 %
 % Note: The first column of W corresponds to the parameters for the bias unit
-%
 
 epsilon_init = 0.12;
 W = rand(L_out, 1 + L_in) * 2 * epsilon_init - epsilon_init;
-
-
-
-
-
-
 
 % =========================================================================
 
