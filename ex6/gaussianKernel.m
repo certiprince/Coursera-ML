3b73bce57f819f1c3ddd2c4439cb472fe05f7e9e
certiprince <57842626+certiprince@users.noreply.github.com>
2020-05-07 14:00:37 +0100

    Cleaned up comments

--- ex6/gaussianKernel.m
+++ ex6/gaussianKernel.m
@@ -9,17 +9,11 @@ x1 = x1(:); x2 = x2(:);
 % You need to return the following variables correctly.
 sim = 0;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the similarity between x1
-%               and x2 computed using a Gaussian kernel with bandwidth
-%               sigma
-%
-%
+% Code to return the similarity between x1
+% and x2 computed using a Gaussian kernel with bandwidth
+% sigma
 
 sim = exp(-((x1-x2)'*(x1-x2)) / (2*sigma^2));
-
-
-
 
 % =============================================================
     
