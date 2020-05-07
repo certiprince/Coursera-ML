0801e7bc1a3d60634e956dc3b4b01c2f3f950fda
certiprince <57842626+certiprince@users.noreply.github.com>
2020-05-07 14:13:22 +0100

    Cleaned up comments

--- ex8/cofiCostFunc.m
+++ ex8/cofiCostFunc.m
@@ -17,28 +17,8 @@ J = 0;
 X_grad = zeros(size(X));
 Theta_grad = zeros(size(Theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost function and gradient for collaborative
-%               filtering. Concretely, you should first implement the cost
-%               function (without regularization) and make sure it is
-%               matches our costs. After that, you should implement the 
-%               gradient and use the checkCostFunction routine to check
-%               that the gradient is correct. Finally, you should implement
-%               regularization.
-%
-% Notes: X - num_movies  x num_features matrix of movie features
-%        Theta - num_users  x num_features matrix of user features
-%        Y - num_movies x num_users matrix of user ratings of movies
-%        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
-%            i-th movie was rated by the j-th user
-%
-% You should set the following variables correctly:
-%
-%        X_grad - num_movies x num_features matrix, containing the 
-%                 partial derivatives w.r.t. to each element of X
-%        Theta_grad - num_users x num_features matrix, containing the 
-%                     partial derivatives w.r.t. to each element of Theta
-%
+% Compute the cost function and gradient for collaborative
+% filtering.
 
 % Compute unregularised cost function
 pred = X*Theta';
@@ -59,12 +39,12 @@ reg_X = 0.5*lambda*sum(sum(X.^2));
 
 % Regularised cost function
 J = J + reg_Theta + reg_X;
-% =============================================================
 
 % Regularised gradient
 X_grad = X_grad + lambda*X;
 Theta_grad = Theta_grad + lambda*Theta;
 
+% =============================================================
 
 grad = [X_grad(:); Theta_grad(:)];
 
